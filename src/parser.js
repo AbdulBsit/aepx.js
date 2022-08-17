4b91f419a668996a54913ed596532f027ca84c25
Abdul Basit <abasit325@gmail.com>
2022-08-17 18:13:17 +0530

    fix

--- src/parser.js
+++ src/parser.js
@@ -113,7 +113,7 @@ function parseLayer(layer) {
     //if layer is text then fetch properties
     if (result.ldta.type == 1) {
       // result.ldta.text=(layer?.tdgp[0]?.tdgp[0]?.btds[0]?.tdbs[0]?.string??"")[0]
-       result.ldta.font = hexConverter.hexToAsciiString((((((layer?.tdgp ?? [])[0]?.tdgp ?? [])[0]?.btds ?? [])[0]?.btdk ?? [])[0]?.$?.bdata || "") || "")?.split("/0 << /0 (þÿ")[1]?.split(")")[0]?.replace(/\u0000/gi, "") || ""
+      result.ldta.font = hexConverter.hexToAsciiString((((((layer?.tdgp ?? [])[0]?.tdgp ?? [])[0]?.btds ?? [])[0]?.btdk ?? [])[0]?.$?.bdata || "") || "")?.split("/0 << /0 (þÿ")[1]?.split(")")[0]?.replace(/\u0000/gi, "") || ""
     }
   }
   // 
